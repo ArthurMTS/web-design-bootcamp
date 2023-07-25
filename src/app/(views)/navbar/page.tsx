ede17cfe383e7c956b5ee42d9ca0741b5ef609cc
Arthur Silva <arthurmatsilva@outlook.com>
2023-07-25 14:35:52 -0300

    vid #4 responsive navbar

---
+++ src/app/(views)/navbar/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useState } from "react";
+import "./styles.css";
+
+export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const toggleMenuOpen = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <main className={`${open ? "open" : ""}`}>
+      <nav className="navbar">
+        <div className="navbar-overlay" onClick={toggleMenuOpen}></div>
+        <button className="navbar-burger" onClick={toggleMenuOpen}>
+          <i className="bx bx-menu"></i>
+        </button>
+
+        <h1 className="navbar-title">AsmrProg</h1>
+        <nav className="navbar-menu">
+          <button className="active">Youtube</button>
+          <button className="active">Archive</button>
+          <button>Projects</button>
+        </nav>
+      </nav>
+    </main>
+  );
+}
