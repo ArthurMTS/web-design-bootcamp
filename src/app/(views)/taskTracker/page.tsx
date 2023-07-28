a7f712143fbeacd8ea578f05c9d636828e83691a
Arthur Silva <arthurmatsilva@outlook.com>
2023-07-28 14:15:25 -0300

    vid #10 responsive task tracker

---
+++ src/app/(views)/taskTracker/page.tsx
@@ -0,0 +1,303 @@
+import "./styles.css";
+
+export default function TaskTracker() {
+  return (
+    <main>
+      <h2>Task Tracker</h2>
+      <div className="container">
+        <div className="box-column">
+          <div className="box-header">
+            <h5>
+              Backlog <span className="num">8</span>
+            </h5>
+            <li>
+              <i className="bx bx-plus"></i>
+            </li>
+          </div>
+
+          <div className="box">
+            <span className="tag" id="red">
+              Youtube
+            </span>
+            <p>
+              This is the title of the card for the thing that needs to be done.
+            </p>
+            <div className="box-footer">
+              <div className="date">
+                <li>
+                  <i className="bx bxs-calendar"></i>
+                </li>
+                <span>8 May</span>
+              </div>
+
+              <li className="comment">
+                <i className="bx bxs-message"></i>14
+              </li>
+            </div>
+          </div>
+
+          <div className="box">
+            <span className="tag" id="green">
+              Dev
+            </span>
+            <p>
+              This is the title of the card for the thing that needs to be done.
+            </p>
+            <div className="box-footer">
+              <div className="date">
+                <li>
+                  <i className="bx bxs-calendar"></i>
+                </li>
+                <span>8 May</span>
+              </div>
+
+              <li className="comment">
+                <i className="bx bxs-message"></i>14
+              </li>
+            </div>
+          </div>
+
+          <div className="box">
+            <span className="tag" id="yellow">
+              Design
+            </span>
+            <p>
+              This is the title of the card for the thing that needs to be done.
+            </p>
+            <div className="box-footer">
+              <div className="date">
+                <li>
+                  <i className="bx bxs-calendar"></i>
+                </li>
+                <span>8 May</span>
+              </div>
+
+              <li className="comment">
+                <i className="bx bxs-message"></i>14
+              </li>
+            </div>
+          </div>
+        </div>
+        <div className="box-column">
+          <div className="box-header">
+            <h5>
+              Ready <span className="num">9</span>
+            </h5>
+            <li>
+              <i className="bx bx-plus"></i>
+            </li>
+          </div>
+
+          <div className="box">
+            <span className="tag" id="red">
+              Youtube
+            </span>
+            <p>
+              This is the title of the card for the thing that needs to be done.
+            </p>
+            <div className="box-footer">
+              <div className="date">
+                <li>
+                  <i className="bx bxs-calendar"></i>
+                </li>
+                <span>8 May</span>
+              </div>
+
+              <li className="comment">
+                <i className="bx bxs-message"></i>14
+              </li>
+            </div>
+          </div>
+
+          <div className="box">
+            <span className="tag" id="green">
+              Dev
+            </span>
+            <p>
+              This is the title of the card for the thing that needs to be done.
+            </p>
+            <div className="box-footer">
+              <div className="date">
+                <li>
+                  <i className="bx bxs-calendar"></i>
+                </li>
+                <span>8 May</span>
+              </div>
+
+              <li className="comment">
+                <i className="bx bxs-message"></i>14
+              </li>
+            </div>
+          </div>
+
+          <div className="box">
+            <span className="tag" id="yellow">
+              Design
+            </span>
+            <p>
+              This is the title of the card for the thing that needs to be done.
+            </p>
+            <div className="box-footer">
+              <div className="date">
+                <li>
+                  <i className="bx bxs-calendar"></i>
+                </li>
+                <span>8 May</span>
+              </div>
+
+              <li className="comment">
+                <i className="bx bxs-message"></i>14
+              </li>
+            </div>
+          </div>
+        </div>
+        <div className="box-column">
+          <div className="box-header">
+            <h5>
+              Doing <span className="num">4</span>
+            </h5>
+            <li>
+              <i className="bx bx-plus"></i>
+            </li>
+          </div>
+
+          <div className="box">
+            <span className="tag" id="red">
+              Youtube
+            </span>
+            <p>
+              This is the title of the card for the thing that needs to be done.
+            </p>
+            <div className="box-footer">
+              <div className="date">
+                <li>
+                  <i className="bx bxs-calendar"></i>
+                </li>
+                <span>8 May</span>
+              </div>
+
+              <li className="comment">
+                <i className="bx bxs-message"></i>14
+              </li>
+            </div>
+          </div>
+
+          <div className="box">
+            <span className="tag" id="green">
+              Dev
+            </span>
+            <p>
+              This is the title of the card for the thing that needs to be done.
+            </p>
+            <div className="box-footer">
+              <div className="date">
+                <li>
+                  <i className="bx bxs-calendar"></i>
+                </li>
+                <span>8 May</span>
+              </div>
+
+              <li className="comment">
+                <i className="bx bxs-message"></i>14
+              </li>
+            </div>
+          </div>
+
+          <div className="box">
+            <span className="tag" id="yellow">
+              Design
+            </span>
+            <p>
+              This is the title of the card for the thing that needs to be done.
+            </p>
+            <div className="box-footer">
+              <div className="date">
+                <li>
+                  <i className="bx bxs-calendar"></i>
+                </li>
+                <span>8 May</span>
+              </div>
+
+              <li className="comment">
+                <i className="bx bxs-message"></i>14
+              </li>
+            </div>
+          </div>
+        </div>
+        <div className="box-column">
+          <div className="box-header">
+            <h5>
+              Review <span className="num">2</span>
+            </h5>
+            <li>
+              <i className="bx bx-plus"></i>
+            </li>
+          </div>
+
+          <div className="box">
+            <span className="tag" id="red">
+              Youtube
+            </span>
+            <p>
+              This is the title of the card for the thing that needs to be done.
+            </p>
+            <div className="box-footer">
+              <div className="date">
+                <li>
+                  <i className="bx bxs-calendar"></i>
+                </li>
+                <span>8 May</span>
+              </div>
+
+              <li className="comment">
+                <i className="bx bxs-message"></i>14
+              </li>
+            </div>
+          </div>
+
+          <div className="box">
+            <span className="tag" id="green">
+              Dev
+            </span>
+            <p>
+              This is the title of the card for the thing that needs to be done.
+            </p>
+            <div className="box-footer">
+              <div className="date">
+                <li>
+                  <i className="bx bxs-calendar"></i>
+                </li>
+                <span>8 May</span>
+              </div>
+
+              <li className="comment">
+                <i className="bx bxs-message"></i>14
+              </li>
+            </div>
+          </div>
+
+          <div className="box">
+            <span className="tag" id="yellow">
+              Design
+            </span>
+            <p>
+              This is the title of the card for the thing that needs to be done.
+            </p>
+            <div className="box-footer">
+              <div className="date">
+                <li>
+                  <i className="bx bxs-calendar"></i>
+                </li>
+                <span>8 May</span>
+              </div>
+
+              <li className="comment">
+                <i className="bx bxs-message"></i>14
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
